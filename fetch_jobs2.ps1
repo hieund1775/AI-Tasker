d593e89c819dbf7bf46f8eeaf36a737bef475774
FPTxNDT <nthanh240205@gmail.com>
2026-07-10 16:28:55 +0700

    xóa các file rác: fetch_jobs scripts và frontend.zip

--- fetch_jobs2.ps1
+++
@@ -1,2 +0,0 @@
-$res = Invoke-RestMethod -Uri 'https://unoverthrown-unspuriously-leyla.ngrok-free.dev/api/JobPosts'
-$res | Select-Object id, title, status | ConvertTo-Json
--- fetch_jobs_railway.ps1
+++
@@ -1,2 +0,0 @@
-$res = Invoke-RestMethod -Uri 'https://aitaskerbe-production.up.railway.app/api/JobPosts'
-$res | Select-Object id, title, status | ConvertTo-Json
